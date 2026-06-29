1269b4688cc94cbd271e1cbbf19a6e7caa2293de
bbedward <bbedward@gmail.com>
2026-06-29 08:27:06 -0400

    fix pre-commit

--- internal/server/http.go
+++ internal/server/http.go
@@ -53,7 +53,6 @@ func NewHTTP(addr string, indexer api.IndexerInterface, watcher api.WatcherInter
 
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.RequestID)
-		r.Use(middleware.RealIP)
 		r.Use(middleware.Logger)
 		r.Use(middleware.Recoverer)
 		r.Use(middleware.Timeout(30 * time.Second))
